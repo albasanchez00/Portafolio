0da4d60e41ef3c2459c9d47b8e9f69b5607926d9
Alba Sánchez <albasanchezolmedo00@gmail.com>
2025-01-09 01:35:02 +0100

    Header y footer

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<?php
+include_once 'page/header.php';
+?>
+    <section class="section-body">
+        <section class="section-presentacion">
+            <div>
+                <h2 class="text-center">Bienvenidos/as</h2>
+                <p>Soy un desarrollador digital con 22 años de experiencia en el fascinante mundo de la informática y la programación. Me apasiona profundizar en múltiples disciplinas como programación, diseño, marketing, SEO, filosofía, creación de contenido y ciencia, abordándolas siempre con curiosidad y rigor técnico.</p>
 
+            </div>
+            <picture><img src="media/img-alba-cv.jpg" alt=""></picture>
+        </section>
 
+        <section>
 
+        </section>
+
+        <section>
+
+        </section>
+    </section>
+
+<?php
+include_once 'page/footer.php';
+?>
