11a952c92b9828322b9d323eb4cb3e5d693af013
Alba Sánchez <albasanchezolmedo00@gmail.com>
2025-01-10 16:27:04 +0100

    Creacion de Contacto

--- contacto.php
+++ contacto.php
@@ -1,48 +1,34 @@
 <?php
 include_once 'header.php';
+include_once 'lectura.php';
 ?>
 
 <section class="section-body">
-    <section>
-        <div>
-            <form action="lectura.php">
-                <p>
-                    <label for="nome">Nombre:</label>
-                    <input type="text">
+    <section class="section-form">
+        <form action="lectura.php" method="post" autocomplete="on">
+            <h1>Contacto</h1>
+            <div class="group-form">
+                <p class="col-form">
+                    <label for="nombre">Nombre:</label>
+                    <input type="text" name="nombre" id="nombre">
                 </p>
-                <p>
-                    <label for=""></label>
-                    <input type="text">
+                <p class="col-form">
+                    <label for="email">Email:</label>
+                    <input type="email" name="email" id="email" placeholder="example@dominio.com">
                 </p>
-                <p>
-                    <label for=""></label>
-                    <input type="text">
+                <p class="col-form">
+                    <label for="tlfn">Asunto:</label>
+                    <input type="text" name="tlfn" id="tlfn">
                 </p>
-                <p>
-                    <label for=""></label>
-                    <input type="text">
-                </p>
-
-            </form>
-        </div>
+            </div>
+            <p class="col-form-contact">
+                <label for="mensaje"></label>
+                <textarea name="mensaje" id="mensaje" cols="30" rows="10" placeholder="Motivo de mensaje"></textarea>
+            </p>
+            <input type="submit" value="Enviar" id="btn-enviar" class="btn-proyectos">
+        </form>
     </section>
-
-
-
 </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
 <?php
 include_once 'footer.php';
 ?>
--- lectura.php
+++ lectura.php
@@ -1,1 +1,36 @@
 <?php
+//Reseteamos variables a 0.
+$nombre = $email = $subject = $mensaje = $para = $headers = $msjCorreo = NULL;
+
+if (isset($_POST['submit'])) {
+    //Obtenemos valores input formulario
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $subject = $_POST['asunto'];
+    $mensaje = $_POST['mensaje'];
+    $para = 'albasanchezolmedo00@gmail.com';
+
+    //Creamos cabecera.
+    $headers = 'From' . " " . $email . "\r\n";
+    $headers .= "Content-type: text/html; charset=utf-8";
+
+    //Componemos cuerpo correo.
+    $msjCorreo = "Nombre: " . $nombre;
+    $msjCorreo .= "\r\n";
+    $msjCorreo .= "Email: " . $email;
+    $msjCorreo .= "\r\n";
+    $msjCorreo .= "Asunto: " . $subject;
+    $msjCorreo .= "\r\n";
+    $msjCorreo .= "Mensaje: " . $mensaje;
+    $msjCorreo .= "\r\n";
+
+    if (mail($para, $subject, $msjCorreo, $headers)) {
+        echo "<script language='javascript'>
+            alert('Mensaje enviado, muchas gracias.');
+         </script>";
+    } else {
+        echo "<script language='javascript'>
+            alert('fallado');
+         </script>";
+    }
+}
